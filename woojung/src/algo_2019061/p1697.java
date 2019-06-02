a2c93c1968455990c14f830ea5e3adf4fcb210ea
dnwjdrnl <32255382+dnwjdrnl@users.noreply.github.com>
2019-06-02 13:01:59 +0900

    우정 commit

---
+++ woojung/src/algo_2019061/p1697.java
@@ -0,0 +1,47 @@
+package algo_2019061;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class p1697 {
+
+	static final int MAX=100000;
+	static int N;
+	static int K;
+	static int[] dx= {-1,1,2};
+	static int[] visited;
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		visited=new int[MAX+1];
+		N=sc.nextInt();
+		K=sc.nextInt();
+		Queue<Integer> qq=new LinkedList<Integer>();
+		visited[N]=1;
+		qq.add(N);
+		qq.add(0);
+		
+		while(!qq.isEmpty()) {
+			int nN=qq.remove();
+			int cnt=qq.remove();
+			if(nN==K) {
+				System.out.println(cnt);	
+				break;
+			}
+			
+			for(int i=0;i<3;i++) {
+				int nnN;
+				if(i==2) nnN=dx[i]*nN;
+				else nnN=dx[i]+nN;
+				
+				if(nnN>=0&&nnN<=MAX&&visited[nnN]!=1) {
+					qq.add(nnN);
+					qq.add(cnt+1);
+					visited[nnN]=1;
+				}
+				
+			}
+		}
+		sc.close();
+	}
+	
+}
---
+++ woojung/src/algo_2019061/p2178.java
@@ -0,0 +1,75 @@
+package algo_2019061;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class p2178 {
+	static int N;
+	static int M;
+	static int[][] map;
+	static int[] dx= {-1,1,0,0};
+	static int[] dy= {0,0,-1,1};
+	static int result;
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		N=sc.nextInt();
+		M=sc.nextInt();
+		map=new int[N+1][M+1];
+		for(int i=1;i<N+1;i++) {
+			String line=sc.next();
+			for(int j=0;j<M;j++) {
+				map[i][j+1]=line.charAt(j)-'0';
+			}
+		}
+		
+		result=bfs();
+		System.out.println(result);
+		
+		sc.close();
+	}
+	
+	private static int bfs() {
+		int row=1;
+		int col=1;
+		int cnt=1;
+		Queue<Integer> qq=new LinkedList<Integer>();
+		qq.add(row);
+		qq.add(col);
+		qq.add(cnt);
+		
+		while(!qq.isEmpty()) {
+			row=qq.remove();
+			col=qq.remove();
+			cnt=qq.remove();
+			
+			if(row==N&&col==M) {
+				return cnt;
+			}
+			
+			for(int i=0;i<4;i++) {
+				int rrow=row+dx[i];
+				int ccol=col+dy[i];
+				
+				if(rrow>0&&rrow<N+1&&ccol>0&&ccol<M+1&&map[rrow][ccol]==1) {
+					map[rrow][ccol]=2;
+					qq.add(rrow);
+					qq.add(ccol);
+					qq.add(cnt+1);
+				}
+			}
+		}
+		
+		return 0;
+	}
+
+	public static void print() {
+		for(int i=1;i<N+1;i++) {
+			for(int j=1;j<M+1;j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
